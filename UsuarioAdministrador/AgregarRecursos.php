a6feddcf8abfa49b012ea3549d0cb9e5f8217cf1
fernandacoto <fermarioro@gmail.com>
2013-11-13 19:53:46 -0600

    Iniciando funcionalidad de recursos

--- UsuarioAdministrador/AgregarRecursos.php
+++ UsuarioAdministrador/AgregarRecursos.php
@@ -6,6 +6,7 @@
 <title>SAC</title>
 <link rel="stylesheet" type="text/css" href="../estilos/EstilosGenericos.css">
 <link rel="stylesheet" type="text/css" href="../estilos/AgregarRecursos.css">
+<script src="../js/Recursos.js"> </script>
 </head>
 
 <body>
@@ -19,11 +20,12 @@
 		<div id="Contenido">
 			<div id="MenuBar" class="MenuBar" >
 				<ul>
-					<li><a href="./PerfilUsuario.html">Perfil</a></li>
+					<li><a href="./AsignarPermisos.html">Permisos</a></li>
+					<li><a href="./AsignarUsuarios.html">Usuarios</a></li>
 					<li><a href="./CrearEventos.html">Eventos</a></li>
-					<li><a href="./VerTipoSesiones.html">Tipos de Sesión</a></li>
-					<li><a href="./VerSalones.html">Salones</a></li>
-					<li><a href="../UsuarioGeneral/IniciarSesion.php" onclick="return cerrarSesion()">Cerrar Sesi&oacute;n</a></li>	
+					<li><a href="./CrearTipoSesion.html">Tipos de Sesión</a></li>
+					<li><a href="./CrearSalon.html">Salones</a></li>				
+					<li><a href="../UsuarioGeneral/IniciarSesion.php" onclick="return cerrarSesion()">Cerrar Sesi&oacute;n</a></li>				
 				</ul>
 			</div>
 		</div> <br>
@@ -51,12 +53,14 @@
 			</table>
 		</div><br>
 		<div id="col2" width="80%">
+			<input type="button" id="Nuevo_recurso" value="Nuevo recurso" onclick="mostrar_formulario()"  class="TipoBoton1" /><br><br>
 			<table align="center">
 			<thead>
 				<tr>
 				<th><label>Recurso</label></th>
 				<th><label>Tipo</label></th>
 				<th><label>Usuario</label></th>
+				<th><label>Eliminar</label></th>
 				<th><label>Ver</label></th>
 				</tr>
 			</thead>
@@ -65,10 +69,27 @@
 				<th><label>Recurso 1</label></th>
 				<th><label>URL</label></th>
 				<th><label>usuario 1</label></th>
+				<th><button id="eliminar" class="TipoBoton2"><img src="../multimedia/BotonEliminar.png" height="30" width="30"></button></th>
 				<th><button id="ver" class="TipoBoton2"><img src="../multimedia/BotonVerDetalle.png" height="30" width="30"></button></th>	
 				</tr>
 			</tbody>
 			</table><br>
+			<div id="formulario_recurso">
+				<label>Tipo de recurso</label><br>
+				<select id="recurso">
+					<option>PDF</option>
+					<option>URL</option>
+				</select><br><br>
+				<input type="button" id="continuar" onclick="mostrar_subform()" value="Continuar" class="TipoBoton1"><br>
+				<div id="escondido1">
+					<label>Cargar archivo</label><input type="file"/>
+					<input type="button" class="TipoBoton3" id="Guardar" value="Guardar" onclick="guardar()">
+				</div><br>
+				<div id="escondido2">
+					<label>Digite la url:</label><input type="text" id="txt_url" /><div id="warning1"><label>No se digit&oacute; la url</label></div>
+					<input type="button" class="TipoBoton3" id="Guardar" value="Guardar" onclick="guardar()">
+				</div>
+			</div>
 		</div>
 	</div>
 </body>
